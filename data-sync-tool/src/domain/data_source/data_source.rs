e7238aae0fd82c1daffdbe4e671a3fd8bdcfb143
Steve Lan <stevelan1995@gmail.com>
2023-02-20 23:43:15 +0800

    used smart pointers

--- data-sync-tool/src/domain/data_source/data_source.rs
+++ data-sync-tool/src/domain/data_source/data_source.rs
@@ -2,39 +2,76 @@
 
 use chrono::prelude::*;
 use uuid::Uuid;
-use std::collections::HashMap;
+use std::{collections::HashMap, cell::RefCell, rc::Rc};
 use fake::{Dummy, Fake};
+use getset::{CopyGetters, Getters, MutGetters, Setters};
 
 use super::dataset::Dataset;
 
-#[derive(Debug, Dummy, PartialEq, Eq, Clone)]
+#[derive(Debug, Dummy, PartialEq, Eq, Clone, Getters, Setters, MutGetters, CopyGetters)]
 #[readonly::make]
 pub struct DataSource {
-    pub id: Uuid,
-    pub name: String,
-    pub description: String,
-    pub api_key: String,
-    pub create_date: DateTime<Utc>, // fixme, Local is not compatible with Dummy
-    pub last_update: Option<DateTime<Utc>>,
-    pub update_successful: Option<bool>,
-    pub datasets: HashMap<String, Dataset>
+    #[getset(get, set, get_mut)]
+    id: Rc<RefCell<Uuid>>,
+    #[getset(get, set, get_mut)]
+    name: Rc<RefCell<String>>,
+    #[getset(get, set, get_mut)]
+    description: Rc<RefCell<String>>,
+    #[getset(get, set, get_mut)]
+    api_key: Rc<RefCell<String>>,
+    #[getset(get, set, get_mut)]
+    create_date: Rc<RefCell<DateTime<Utc>>>, // fixme, Local is not compatible with Dummy
+    #[getset(get, set, get_mut)]
+    last_update: Option<Rc<RefCell<DateTime<Utc>>>>,
+    #[getset(get, set, get_mut)]
+    update_successful: Option<Rc<RefCell<bool>>>,
+    #[getset(get, set, get_mut)]
+    datasets: Rc<RefCell<HashMap<String, Dataset>>>
 }
 
 impl DataSource {
     pub fn new(id: Uuid, name: &str, description: &str, 
                api_key: &str, create_date: DateTime<Utc>,
                last_update: Option<DateTime<Utc>>, update_successful: Option<bool>,
-               datasets: HashMap<String, Dataset>) -> Self {
-        Self {
-            id,
-            name: name.to_string(),
-            description: description.to_string(),
-            api_key: description.to_string(),
-            create_date,
-            last_update,
-            update_successful,
-            datasets
-        }  
+               datasets: Rc<RefCell<HashMap<String, Dataset>>>) -> Self {
+        if let Some(update_dt) = last_update {
+            if let Some(update_ok) = update_successful {
+                Self {
+                    id: Rc::new(RefCell::new(id)),
+                    name: Rc::new(RefCell::new(name.to_string())),
+                    description: Rc::new(RefCell::new(description.to_string())),
+                    api_key: Rc::new(RefCell::new(api_key.to_string())),
+                    create_date: Rc::new(RefCell::new(create_date)),
+                    last_update: Some(Rc::new(RefCell::new(update_dt))),
+                    update_successful: Some(Rc::new(RefCell::new(update_ok))),
+                    datasets: datasets
+                }
+            } else {
+                Self {
+                    id: Rc::new(RefCell::new(id)),
+                    name: Rc::new(RefCell::new(name.to_string())),
+                    description: Rc::new(RefCell::new(description.to_string())),
+                    api_key: Rc::new(RefCell::new(api_key.to_string())),
+                    create_date: Rc::new(RefCell::new(create_date)),
+                    last_update: Some(Rc::new(RefCell::new(update_dt))),
+                    update_successful: None,
+                    datasets: datasets
+                }
+            }
+            
+        } else {
+            Self {
+                id: Rc::new(RefCell::new(id)),
+                name: Rc::new(RefCell::new(name.to_string())),
+                description: Rc::new(RefCell::new(description.to_string())),
+                api_key: Rc::new(RefCell::new(api_key.to_string())),
+                create_date: Rc::new(RefCell::new(create_date)),
+                last_update: None,
+                update_successful: None,
+                datasets: datasets
+            }
+        }
+          
     }
 }
 
@@ -42,7 +79,7 @@ impl DataSource {
 mod test {
     use super::*;
     // use fake::faker::chrono::zh_cn::DateTimeBefore;
-    use fake::faker::lorem::en::{Paragraphs, Words, Paragraph};
+    use fake::faker::lorem::en::Paragraph;
     use fake::faker::name::en::Name;
     use fake::locales::ZH_CN;
     use chrono::Utc;
@@ -60,12 +97,39 @@ mod test {
         let create_date: chrono::DateTime<Utc> = DateTimeBefore(ZH_CN, Utc::now()).fake();
         let last_update: Option<DateTime<Utc>> = None;
         let update: Option<bool> = None;
-        let datasets: HashMap<String, Dataset> = HashMap::new();
+        let datasets: Rc<RefCell<HashMap<String, Dataset>>> = Rc::new(RefCell::new(HashMap::new()));
 
         let empty_datasource = DataSource::new(
-            id,&name, &description, &api_key, create_date, last_update, update, datasets);
+            id,&name, &description, &api_key, create_date, last_update, update, datasets.clone());
+        
+        // test field access
+        assert_eq!(*empty_datasource.id.borrow(), id);
+        assert_eq!(*empty_datasource.name.borrow(), name);
+        assert_eq!(*empty_datasource.description.borrow(), description);
+        assert_eq!(*empty_datasource.api_key.borrow(), api_key);
+        assert_eq!(*empty_datasource.create_date.borrow(), create_date);
+        assert_eq!(empty_datasource.last_update, None);
+        assert_eq!(empty_datasource.update_successful, None);
+        assert_eq!(*empty_datasource.datasets.borrow(), *datasets.borrow());
+    }
 
-        assert_eq!(empty_datasource.id, id);
+    #[test]
+    fn getters_should_return_the_same_value_as_fields() {
+        let fake_datasource: DataSource = Faker.fake();
+        println!("{:?}", fake_datasource);
+        println!("{:?}", *fake_datasource.create_date().borrow());
+        println!("{:?}", *fake_datasource.last_update());
+        println!("{:?}", *fake_datasource.update_successful());
+        
+        // test getters
+        assert_eq!(*fake_datasource.id().borrow(), *fake_datasource.id.borrow());
+        assert_eq!(*fake_datasource.name().borrow(), *fake_datasource.name.borrow());
+        assert_eq!(*fake_datasource.description().borrow(), *fake_datasource.description.borrow());
+        assert_eq!(*fake_datasource.api_key().borrow(), *fake_datasource.api_key.borrow());
+        assert_eq!(*fake_datasource.create_date().borrow(), *fake_datasource.create_date.borrow());
+        assert_eq!(*fake_datasource.last_update(), fake_datasource.last_update);
+        assert_eq!(*fake_datasource.update_successful(), fake_datasource.update_successful);
+        assert_eq!(*fake_datasource.datasets().borrow(), *fake_datasource.datasets.borrow());
     }
 
 }

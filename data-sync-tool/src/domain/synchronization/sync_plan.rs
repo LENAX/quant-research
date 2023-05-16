eeb23a361af6688a739fc934accbbfcb300ae20b
stevelan <stevelan1995@gmail.com>
2023-05-16 16:24:48 +0800

    SyncDomain

--- data-sync-tool/src/domain/synchronization/sync_plan.rs
+++ data-sync-tool/src/domain/synchronization/sync_plan.rs
@@ -6,6 +6,8 @@ use fake::{Dummy, Fake};
 use uuid::Uuid;
 use getset::{Getters, Setters};
 
+use super::sync_task::SyncTask;
+
 // use std::collections::HashMap;
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum SyncFrequency {
@@ -22,20 +24,22 @@ pub enum SyncFrequency {
 // Synchronization Plan
 #[derive(Debug, PartialEq, Eq, Clone, Getters, Setters)]
 #[getset(get = "pub", set = "pub")]
-pub struct SyncPlan {
+pub struct SyncPlan<'a> {
     id: Uuid,
     name: String,
     description: String,
     trigger_time: Option<DateTime<Utc>>,
     frequency: SyncFrequency,
     active: bool,
+    tasks: Vec<SyncTask<'a>>,
     datasource_id: Option<Uuid>,
     datasource_name: Option<String>,
     dataset_id: Option<Uuid>,
     dataset_name: Option<String>,
+    param_template_id: Option<Uuid>,
 }
 
-impl Default for SyncPlan {
+impl Default for SyncPlan<'_> {
     fn default() -> Self {
         return SyncPlan{
             id: Uuid::new_v4(),
@@ -43,21 +47,23 @@ impl Default for SyncPlan {
             description: String::from("Please complete the plan by adding it to a dataset"),
             frequency: SyncFrequency::Daily,
             active: false,
+            tasks: vec![],
             datasource_id: None,
             datasource_name: None,
             dataset_id: None,
             dataset_name: None,
             trigger_time: None,
+            param_template_id: None,
         }
     }
 }
 
-impl SyncPlan {
-    pub fn new(
-        name: &str, description: &str, trigger_time: Option<DateTime<Utc>>,
-        frequency: SyncFrequency, active: bool, datasource_id: Option<Uuid>,
-        datasource_name: &str, dataset_id: Option<Uuid>, dataset_name: &str
-    ) -> SyncPlan {
+impl SyncPlan<'_> {
+    pub fn new<'a>(
+        name: &'a str, description: &'a str, trigger_time: Option<DateTime<Utc>>,
+        frequency: SyncFrequency, active: bool, tasks: Vec<SyncTask<'a>>, datasource_id: Option<Uuid>,
+        datasource_name: &'a str, dataset_id: Option<Uuid>, dataset_name: &'a str, param_template_id: Option<Uuid>,
+    ) -> SyncPlan<'a> {
         SyncPlan {
             id: Uuid::new_v4(),
             name: name.to_string(),
@@ -65,10 +71,12 @@ impl SyncPlan {
             trigger_time,
             frequency,
             active,
+            tasks: tasks,
             datasource_id,
             datasource_name: Some(datasource_name.to_string()),
             dataset_name: Some(dataset_name.to_string()),
             dataset_id,
+            param_template_id
         }
     }
 
---
+++ data-sync-tool/src/domain/synchronization/value_objects/sync_config.rs
@@ -0,0 +1,15 @@
+use getset::{Getters, Setters};
+
+#[derive(Debug, PartialEq, Eq, Clone, Getters, Setters)]
+#[getset(get = "pub", set = "pub")]
+pub struct Quota {
+    max_line_per_request: int,
+    max_request_per_minute: int,
+    daily_limit: int
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Getters, Setters)]
+#[getset(get = "pub", set = "pub")]
+pub struct SyncConfig {
+    sync_quota: Quota
+}

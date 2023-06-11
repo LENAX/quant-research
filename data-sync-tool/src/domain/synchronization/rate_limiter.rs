42dae9ed9a67ab4021f552005e36e54f0666a6c6
stevelan <stevelan1995@gmail.com>
2023-06-11 21:41:02 +0800

    start working on task executor

--- data-sync-tool/src/domain/synchronization/rate_limiter.rs
+++ data-sync-tool/src/domain/synchronization/rate_limiter.rs
@@ -5,8 +5,7 @@ use std::error::Error;
 use uuid::Uuid;
 use super::{value_objects::sync_config::RateQuota, sync_task::SyncTask};
 
-
-pub trait RateLimiter {
-    fn apply_limit(&mut self, quota: &RateQuota, sync_plan_id: Uuid) -> &mut Self;
+pub trait RateLimiter: Sync + Send {
+    fn apply_limit(&mut self, quota: &RateQuota, sync_plan_id: Uuid) -> bool;
     fn can_proceed(&mut self, task: &SyncTask) -> Result<bool, Box<dyn Error>>;
 }

d28f8584152ac533f218fa27eff96995790e00c1
Steve Lan <stevelan1995@gmail.com>
2023-07-13 09:23:35 +0800

    add run script

---
+++ market_quote/run.bat
@@ -0,0 +1,2 @@
+C:/Users/steve/miniconda3/envs/quant-dev/python.exe d:/pprojects/quant-research/market_quote/get_market_quote.py
+@REM C:/Users/steve/miniconda3/envs/quant-dev/python.exe -c "print('hello!')"
